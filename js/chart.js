a72cfa049581924007c5d9bf5121c020a0926f11
alex <alex.wtho@gmail.com>
2019-06-29 19:01:37 +0800

    init

--- js/chart.js
+++ js/chart.js
@@ -29,6 +29,31 @@ $(function() {
       fill: false
     }]
   };
+  var data2 = {
+    labels: ["Payment required", "Official record", "You Missed a Delivery", "Tax refund", "Bogus boss emails"],
+    datasets: [{
+      label: '# of Votes',
+      data: [10, 19, 3, 5, 2, ],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+
+      ],
+      borderWidth: 1,
+      fill: false
+    }]
+  };
   var multiLineData = {
     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
     datasets: [{
@@ -273,7 +298,7 @@ $(function() {
     // This will get the first returned node in the jQuery collection.
     var barChart = new Chart(barChartCanvas, {
       type: 'bar',
-      data: data,
+      data: data2,
       options: options
     });
   }
